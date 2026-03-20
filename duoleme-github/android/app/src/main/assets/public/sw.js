aecd4852d464a636ca6c55a54489dfe721b59fd6
Pope-zZZ <lov3isajoke@foxmail.com>
2026-03-20 13:23:50 +0800

    init

---
+++ duoleme-github/android/app/src/main/assets/public/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'duoleme-v1';
+const urlsToCache = [
+  '.',
+  '夺舍吉位.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
